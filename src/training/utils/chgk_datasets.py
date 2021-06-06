ef53411e836b72556f0b4189dec378d65597f190
Stanislav Lebedev <lebedevsf239@gmail.com>
2021-06-06 17:30:48 +0300

    add max length as dataset parametr

--- src/training/utils/chgk_datasets.py
+++ src/training/utils/chgk_datasets.py
@@ -6,12 +6,12 @@ from typing import List
 class GPT2SmallDataset(Dataset):
     model_name = "sberbank-ai/rugpt3small_based_on_gpt2"
 
-    def __init__(self, txt_path: str):
+    def __init__(self, txt_path: str, max_length: int):
         tokenizer = GPT2TokenizerFast.from_pretrained(self.model_name)
         tokenizer.pad_token = "<pad>"
 
         samples = self._prep_samples(txt_path)
-        self.tokens = tokenizer(samples, padding=True, truncation=True, max_length=206)
+        self.tokens = tokenizer(samples, padding=True, truncation=True, max_length=max_length)
 
         print('Done tokenizing')
 
