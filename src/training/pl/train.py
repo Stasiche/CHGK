9c0775f03e32c17a78711fbb3305280c2354b190
Mogreine <north7311@gmail.com>
2021-06-08 00:11:31 +0300

    Added some parameters for training

--- src/training/pl/train.py
+++ src/training/pl/train.py
@@ -19,7 +19,7 @@ from definitions import ROOT_PATH
 
 
 class GPT2Sber(pl.LightningModule):
-    def __init__(self, model_path: str, lr: float, w_decay: float, warmup_steps: Union[int, float]):
+    def __init__(self, model_path: str, lr: float, w_decay: float, warmup_steps: Union[int, float], eps: float):
         super().__init__()
         self.model = GPT2LMHeadModel.from_pretrained(model_path)
 
@@ -37,6 +37,7 @@ class GPT2Sber(pl.LightningModule):
 
         self.lr = lr
         self.w_decay = w_decay
+        self.eps = eps
 
     @property
     def num_training_steps(self) -> int:
@@ -167,7 +168,7 @@ class GPT2Sber(pl.LightningModule):
             return acc_top1, acc_topk
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.w_decay)
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr, weight_decay=self.w_decay, eps=self.eps)
         if isinstance(self.warmup_steps, float):
             warmup_steps = self.num_training_steps * self.warmup_steps
         else:
@@ -189,7 +190,13 @@ class DataModule(pl.LightningDataModule):
     tokens: Dict[str, List[int]]
 
     def __init__(
-        self, tokenizer_path: str, data_path: str, train_batch_size: int, val_batch_size: int, train_size: float
+        self,
+        tokenizer_path: str,
+        data_path: str,
+        train_batch_size: int,
+        val_batch_size: int,
+        train_size: float,
+        seq_len: int,
     ):
         super().__init__()
 
@@ -197,6 +204,7 @@ class DataModule(pl.LightningDataModule):
         self.tokenizer.pad_token = "<pad>"
 
         self.data_path = data_path
+        self.seq_len = seq_len
 
         self.train_batch_size = train_batch_size
         self.val_batch_size = val_batch_size
@@ -206,7 +214,7 @@ class DataModule(pl.LightningDataModule):
 
     def prepare_data(self) -> None:
         samples = self._prep_samples(self.data_path)
-        self.tokens = self.tokenizer(samples, padding=True, truncation=True, max_length=206)
+        self.tokens = self.tokenizer(samples, padding=True, truncation=True, max_length=self.seq_len)
 
     def setup(self, stage: Optional[str] = None) -> None:
         dataset = [self.tokens["input_ids"], self.tokens["attention_mask"], self.tokens["input_ids"]]
@@ -243,12 +251,14 @@ def train(config: DictConfig) -> None:
         train_batch_size=config.dataset.train_batch_size,
         val_batch_size=config.dataset.val_batch_size,
         train_size=config.dataset.train_size,
+        seq_len=config.dataset.seq_len,
     )
 
     model = GPT2Sber(
         config.model,
         lr=config.training.opt.lr,
         w_decay=config.training.opt.w_decay,
+        eps=config.training.opt.eps,
         warmup_steps=config.training.opt.warmup_steps,
     )
 
