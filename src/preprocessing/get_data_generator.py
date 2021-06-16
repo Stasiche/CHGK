b0d6da546030fdbcf3aa9526c83ac459e47735b7
Stanislav Lebedev <lebedevsf239@gmail.com>
2021-06-16 23:03:15 +0300

    add new data generator

---
+++ src/preprocessing/get_data_generator.py
@@ -0,0 +1,8 @@
+import csv
+from typing import Generator, OrderedDict
+
+def get_data_generator(dataset_path: str) -> Generator[OrderedDict, None, None]:
+    with open(dataset_path) as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            yield row
