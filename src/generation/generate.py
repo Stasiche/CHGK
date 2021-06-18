780265fa23ae4bd3927467258f1a5863a611cc8d
Mogreine <north7311@gmail.com>
2021-06-18 20:46:51 +0300

    Added gpu support for generation

--- src/generation/generate.py
+++ src/generation/generate.py
@@ -1,16 +1,26 @@
 import os
+import time
+
+import torch
 
 from argparse import ArgumentParser
 from src.training.models.GPT2SberSmall import GPT2SberSmall
 from definitions import SBER_MODEL_SMALL
 from transformers import logging
+from typing import Tuple
 
 
-def generate(model_dir: str, tokenizer_path: str, context: str, max_len: int, beam_size: int) -> str:
-    model = GPT2SberSmall(model_dir, tokenizer_path)
+def generate(
+    model_dir: str, tokenizer_path: str, context: str, max_len: int, beam_size: int, device: torch.device
+) -> Tuple[str, float]:
+    model = GPT2SberSmall(model_dir, tokenizer_path, device)
+    model.eval()
+
+    start = time.time()
     generated_text = model.generate(context, max_len, beam_size)
+    elapsed_time = time.time() - start
 
-    return generated_text
+    return generated_text, elapsed_time
 
 
 if __name__ == "__main__":
@@ -25,7 +35,11 @@ if __name__ == "__main__":
     args.add_argument("--context", type=str, required=True, help="Context for generation")
     args.add_argument("--max_len", type=int, required=False, default=50, help="Max length of the output in tokens")
     args.add_argument("--beam_size", type=int, required=False, default=5, help="Beam width")
+    args.add_argument("--device", type=str, default="cpu", help="Device on which to run the model")
     args = args.parse_args()
 
-    generated_text = generate(args.model_dir, args.tokenizer_path, args.context, args.max_len, args.beam_size)
+    generated_text, elapsed_time = generate(
+        args.model_dir, args.tokenizer_path, args.context, args.max_len, args.beam_size, args.device
+    )
     print(generated_text)
+    print(f"Elapsed time: {elapsed_time: .3f}")
--- src/training/models/GPT2SberSmall.py
+++ src/training/models/GPT2SberSmall.py
@@ -4,10 +4,13 @@ import torch
 
 
 class GPT2SberSmall(torch.nn.Module):
-    def __init__(self, model_dir: str, tokenizer_path: str):
+    def __init__(self, model_dir: str, tokenizer_path: str, device: torch.device):
         super().__init__()
         self.model: GPT2LMHeadModel = GPT2LMHeadModel.from_pretrained(model_dir)
         self.tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
+
+        self.model.to(device)
+        self.device = device
 
         self.tokenizer.pad_token = "<pad>"
         self.tokenizer.bos_token = "<s>"
@@ -19,14 +22,12 @@ class GPT2SberSmall(torch.nn.Module):
     def forward(self, *args, **kwargs):
         return self.model.forward(*args, **kwargs)
 
+    @torch.no_grad()
     def generate(self, context: str, max_length: int, beam_size: int):
-
-        input_ids = self.tokenizer.encode(context, return_tensors='pt')
-        greedy_output = self.model.generate(input_ids,
-                                            max_length=max_length,
-                                            beam_size=beam_size,
-                                            no_repeat_ngram_size=2,
-                                            early_stopping=True)
+        input_ids = self.tokenizer.encode(context, return_tensors="pt").to(self.device)
+        greedy_output = self.model.generate(
+            input_ids, max_length=max_length, beam_size=beam_size, no_repeat_ngram_size=2, early_stopping=True
+        )
         generated_output = self.tokenizer.decode(greedy_output[0], skip_special_tokens=True)
 
         return generated_output
